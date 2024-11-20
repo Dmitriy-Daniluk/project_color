3e603989e8c069dce2e312c7ec87a5f30c3a3b76
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2024-11-20 10:39:03 +0300

    Пункт 4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Крансая страница */
+router.get('/red', function(req, res, next) {
+	res.send("<h1>Красная страница</h1>")
+});
+
+/* Синяя страница */
+router.get('/blue', function(req, res, next) {
+	res.send("<h1>Синяя страница</h1>")
+});
+
+/* Зелённая страница */
+router.get('/green', function(req, res, next) {
+	res.send("<h1>Зелёная страница</h1>")
+});
+
+
+
 module.exports = router;
