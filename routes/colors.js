59d8751a9b4b70c728e88cea767980828cc9a573
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-01-18 19:04:01 +0300

    c_10_8

--- routes/colors.js
+++ routes/colors.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Color = require('../models/color').Color;
+var checkAuth = require("../middlewares/checkAuth.js")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница котов */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
 	var colors = await Color.find({nick: req.params.nick});
 	console.log(colors)
 	if(!colors.length) return next(new Error("Нет такого цвета в базе сайта"))
